1d7f3fea023f8495a7cdd9baf8721543faf1783a
Simranjeet kaur <sgill9@hawk.iit.edu>
2024-05-22 11:30:27 -0500

    updated

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,10 +11,18 @@ let render_projects = (slug) => {
     let projects_obj = [
         {
             image: 'assets/images/sql.png',
-            pdf: 'assets/pdf/Project1.pdf',
+            pdf: 'https://public.tableau.com/app/profile/simranjeet.gill/vizzes',
             title: 'Flight Reservation And Booking System',
             technologies: ['SQL', 'EXCEL'],
             description: "Create a flight reservation function by loading and to create DB Tables and executing DDL and DML queries. Designed ER Diagrams, and conceptual models, created database objects, and wrote SQL queries to normalize and populate data into normalized tables from the provided raw files. Generated test data to simulate Passenger details, flight details, and available seats.",
+            categories: ['featured', 'Project']
+        },
+        {
+            image: 'assets/images/hr.jpeg',
+            pdf: 'assets/pdf/HR.png',
+            title: 'HR Analytics Dashboard',
+            technologies: ['Tableau', 'EXCEL'],
+            description: "•	To create a Dashboard that provides comprehensive insights into workforce data, enhancing HR decision-making and operational efficiency.",
             categories: ['featured', 'Project']
         },
         {
